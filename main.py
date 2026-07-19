24874d8f5efef692c599e98679369d5d10b48fe5
mayaannkkk <goyalmayank492@gmail.com>
2026-07-19 17:28:52 +0530

    Sorting Patient Records

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -28,5 +28,18 @@ def view_patient(patient_id : str =  Path(..., description='This is the ID of th
 
     if patient_id in data:
         return data[patient_id]
-    else:
-        return {'error' : 'patient not found'}
+    raise HTTPException(status_code=404, detail="Patient Not Found")
+
+@app.get('/sort')
+def sort_patients(sort_by:str= Query(...,description='Sort on the basis of height, weight or bmi'), order: str= Query("asc", description="Sort in asc or desc")):
+    
+    valid_fields = ['height', 'weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid Request, select form {valid_fields}')
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail=f"Invalid field, Select from {'asc','desc'}")
+    data = load_data()
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values(), key = lambda x : x.get(sort_by,0), reverse=sort_order)
+    return sorted_data  
