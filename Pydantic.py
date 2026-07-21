2c039c49f69df665355994e913abd00c3cb374c0
mayaannkkk <goyalmayank492@gmail.com>
2026-07-22 00:06:30 +0530

    Pydantic.py

--- Pydantic.py
+++ Pydantic.py
@@ -1,19 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, AnyUrl, EmailStr, Field
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
 
-    name : str
-    age: int
-    weight : float
+    name : Annotated[str, Field(max_length=50, title='Name of the patient', description='less than 50 chars', examples=['mayank', 'goyal'])]
+    age: int = Field( gt=0, lt=120)
+    weight : Annotated[float,Field(gt=0, strict=True)] 
+    linkedin_url : Annotated[AnyUrl, Field(default=None)]
+    married : Annotated[bool, Field(default=None, description='Is the patient married or not')]
+    allergies : Optional[List[str]] = None
+    contact_details : Dict[str, str]
 
 def insert_patient(patient : Patient):
 
     print(patient.name)
     print(patient.age)
+    print(patient.allergies)
     print("Data Inserted")    
 
+def update_patient(patient : Patient):
 
-patient_info = {'name' : 'Mayank', 'age':21}
+    print(patient.name)
+    print(patient.age)
+    print("Updated")    
+
+
+patient_info = {'name' : 'Mayank', 'age':21, 'weight' : 65.3, 'married':True, 
+                'contact_details':{'email':'goyalmayank@gmail.com','Mob No': '43413'}}
 
 patient1= Patient(**patient_info)
 
