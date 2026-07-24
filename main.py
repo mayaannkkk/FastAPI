46d7981685d70a56fd5b1d7637e67c7b113d20d6
mayaannkkk <goyalmayank492@gmail.com>
2026-07-24 12:51:47 +0530

    Pydantic model Added for PMS

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 import json
+from pydantic import BaseModel,Field, computed_field
+from typing import Annotated, Literal
 
 app = FastAPI()
 
+class Pateint(BaseModel):
+
+    id : Annotated[str, Field(..., description="Id of the patient", examples="P001")]
+    name : Annotated[str, Field(..., description="Name of the patient")]
+    city : Annotated[str, Field(..., description="City where patient live")]
+    age : Annotated[int, Field(..., gt =0 , lt = 120, description="Id of the patient")]
+    gender : Annotated[Literal['male', 'female', 'other'], Field(..., description="Gender of the patient")]
+    height : Annotated[float, Field(..., gt=0, description="Height of the patient in mtrs")]
+    weight : Annotated[float, Field(..., gt=0, description="weight of the patient in kgs")]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = self.weight / self.height * 2
+        return bmi
+
+    @computed_field
+    @property
+    def verdict(self) -> str:
+
+        if self.bmi < 18.5:
+            return 'UnderWeight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
+        
 def load_data():
     with open('pateints.json', 'r') as f:
         data = json.load(f)
